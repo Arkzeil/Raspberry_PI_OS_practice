f948d41279adcf6d2cf4ac26c7d6c43a361dc1d7
Arkzeil <75022292+Arkzeil@users.noreply.github.com>
2023-09-13 22:09:24 +0800

    with full comments

--- kernel.c
+++ kernel.c
@@ -10,7 +10,7 @@ static inline void mmio_write(uint32_t reg, uint32_t data){ //MMIO(Memory Mapped
     return *(volatile uint32_t*)reg = data; 
 }
 
-static inline void mmio_read(uint32_t reg){
+static inline uint32_t mmio_read(uint32_t reg){
     return *(volatile uint32_t*)reg;
 }
 
